6ee1b47de878c8df01e01f949bc260174ac68336
kees zwart <kees.zwart@hcs-company.com>
2021-03-27 21:01:40 +0100

    iets

--- case-python/app.py
+++ case-python/app.py
@@ -1,9 +1,9 @@
 import time
  
 # Wait for  seconds
-time.sleep(500)
+time.sleep(30)
 print("HALLO")
 # Wait for 300 milliseconds
 # .3 can also be used
-time.sleep(.300)
+time.sleep(.30)
 print("Hello, World!")
